bad582a11d43a12ca7733fee32af620ae69bd383
snjava <scnaikwadi@yahoo.com>
2021-12-20 11:02:42 +0530

    Revision Session-1
    Revision Session-1

---
+++ code/corejava/basics/MyFirstJava.java
@@ -0,0 +1,24 @@
+public class MyFirstJava
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello, This is my First Java Program");
+	}  
+}
+
+
+class MySecondJava 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, This is my Second Java Program");	
+	}
+}
+
+class MyThirdJava 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, This is my Third Java Program");	
+	}
+}
