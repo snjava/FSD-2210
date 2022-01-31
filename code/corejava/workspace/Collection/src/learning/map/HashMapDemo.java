72e3bfc84119c40b8553d2c9cfdbdfa651b6ffc3
snjava <scnaikwadi@yahoo.com>
2022-01-31 11:02:30 +0530

    Collection map Interface
    Collection map Interface

---
+++ code/corejava/workspace/Collection/src/learning/map/HashMapDemo.java
@@ -0,0 +1,38 @@
+package learning.map;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+	public static void main(String[] args) {
+		HashMap map = new HashMap();
+		map.put("Abc", 12123);
+		map.put(234.23, "xyz");
+		map.put("Abc", 'z');
+		map.put("Pqr", 12.123);
+		map.put('z', 12123);
+		map.put(true, 12123);
+		map.put(null, 12123);
+		map.put("xyz", null);
+		map.put(true, null);
+		
+		System.out.println(map);
+		
+		map.remove("Abc"); // remove a single entry which has matching key
+		//map.remove("Abc", 'z'); // remove a single entry which has matching key and value
+		
+		System.out.println(map);
+		
+		System.out.println(map.get('z'));
+		
+		System.out.println(map.containsValue(12.123)); // true
+		System.out.println(map.containsKey(12123)); // false
+		
+		System.out.println(map.size());
+		
+		map.clear();
+		System.out.println(map);
+		System.out.println(map.isEmpty());
+		
+		
+	}
+}
