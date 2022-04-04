f3da586d017215a863a7926198138a47376d0aac
scn-tech <scnaikwadi2@gmail.com>
2022-04-04 11:05:09 +0530

    Spring Boot Intro
    Spring Boot Intro

---
+++ code/spring/sb-intro/src/main/java/com/institute/bean/Student.java
@@ -0,0 +1,10 @@
+package com.institute.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	public Student() {
+		System.out.println("Student Created.......");
+	}
+}
